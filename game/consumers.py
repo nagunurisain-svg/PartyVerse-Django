9f89761b5527423fc3cbe8f859175bbc4b4dd3f2
Nagunuri <arunsaiarunsai320@Gmail.com>
2026-09-06 10:15:57 +0530

    Fix websocket consumer

--- game/consumers.py
+++ game/consumers.py
@@ -3,8 +3,6 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 
-from .models import OnlineRoom
-
 
 class GameConsumer(AsyncWebsocketConsumer):
 
